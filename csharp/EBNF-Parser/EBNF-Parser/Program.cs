7b7bd8c74a4e3191d1ba5ba4b7e2d93497e41e87
mki3 <mki3>
2016-07-01 11:32:10 +0200

    last

--- csharp/EBNF-Parser/EBNF-Parser/Program.cs
+++ csharp/EBNF-Parser/EBNF-Parser/Program.cs
@@ -1,5 +1,12 @@
-﻿using System;
+﻿///<summary>
+///Created on 24.06.2016
+/// @author Group 6
+/// 
+/// </summary>
+
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -9,42 +16,18 @@ namespace EBNF_Parser
 {
     class Program
     {
-        // Main method and Inizialisation of the Threadpool
-        static void Main(string[] args)
+     
+        static void Main (string[] args)
         {
-            string[] exp = System.IO.File.ReadAllLines("C:/expressions1.txt");
+            var reader = new Reader(@"../expression.txt");
+                
+                ThreadPool.SetMaxThreads(3, 3);
+                ThreadPool.SetMinThreads(3, 3);
 
-            int Expressions = exp.Length;
+                System.Threading.ThreadPool.QueueUserWorkItem(reader.parseDocument);
 
-            // Every event is for each Parserobject
-            ManualResetEvent[] doneEvents = new ManualResetEvent[Expressions];
-            Parser[] parsArray = new Parser[Expressions];
-
-            // Starting the Threads with a Threadpool
-            Console.WriteLine("launching {0} tasks...", Expressions);
-            for (int i = 0; i < Expressions; i++)
-            {
-                doneEvents[i] = new ManualResetEvent(false);
-                Parser pars = new Parser(exp[i], doneEvents[i]);
-                parsArray[i] = pars;
-                ThreadPool.QueueUserWorkItem(pars.ThreadPoolCallback, i);
-
-            }
-
-            // Wait until all ParserEvents are done
-            WaitHandle.WaitAll(doneEvents);
-            Console.WriteLine("All Expressions are parsed");
-
-            // Output of the results
-            for (int i = 0; i < Expressions; i++)
-            {
-                Parser p = parsArray[i];
-                Console.WriteLine("Expression({0}) = {1}", p.getText, p.getResult);
-            }
             Console.ReadKey();
-
         }
-
 
     }
 }
