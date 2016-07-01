4018e065d7f3e2d9a07f92d779ea87583c0c7cca
mki3 <mki3>
2016-07-01 10:33:37 +0200

    ralative path

---
+++ EBNF-Parser/EBNF-Parser/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EBNF_Parser
+{
+    class Program
+    {
+        // Main method and Inizialisation of the Threadpool
+        static void Main(string[] args)
+        {
+
+
+           
+
+            var reader = new Reader(@"../expressions.txt");
+
+            ThreadPool.SetMaxThreads(3, 3);
+            ThreadPool.SetMinThreads(3, 3);
+
+            System.Threading.ThreadPool.QueueUserWorkItem(reader.parseDocument);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ EBNF-Parser/EBNF-Parser/Reader.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBNF_Parser
+{
+    public class Reader
+    {
+        static StreamReader sr;
+        static Parser p = new Parser();
+        static bool isClosed = false;
+        static bool done = false;
+
+        public Reader(string s)
+        {
+            sr = new StreamReader(s);
+        }
+
+        public void parseDocument(Object stateInfo)
+        {
+            string s = "";
+            while (!done)
+            {
+                if (!isClosed)
+                {
+                    if ((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(p.parse(s) + " : " + s);
+                    }
+                    else
+                    {
+                        sr.Close();
+                        isClosed = true;
+                        done = true;
+                    }
+                }
+
+            }
+        }
+
+        static string getLine()
+        {
+            string res = null;
+            string s;
+            if (!isClosed)
+            {
+                if ((s = sr.ReadLine()) != null)
+                {
+                    res = s;
+                }
+                else
+                {
+                    sr.Close();
+                    isClosed = true;
+                    done = true;
+                }
+            }
+
+            return res;
+        }
+    }
+
+}
